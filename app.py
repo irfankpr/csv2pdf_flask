835bdfd5beb5fdc7cb7933c23bd37671139fdf8c
irfankpr <irfanmuhammedkpr@gmail.com>
2024-05-17 15:18:42 +0530

    changes in lauout as client req

--- app.py
+++ app.py
@@ -36,6 +36,13 @@ def upload_file():
             if file:
                 file_content = file.stream.read().decode("utf-8")
                 json_data = csv_to_json(file_content)
+                for order in json_data.values():
+                    Tweight = sum(float(item['Qty']) * float(item['Weight']) for item in order)
+                    Tqty = sum(int(item['Qty']) for item in order)
+                    Tprice = sum(float(item['Qty']) * float(item['Price']) for item in order)
+                    order[0]['Tweight'] = round(Tweight, 3)
+                    order[0]['Tqty'] = Tqty
+                    order[0]['Tprice'] = Tprice
                 return render_template('result.html', json_data=json_data)
 
         return render_template('index.html')
@@ -45,4 +52,4 @@ def upload_file():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
